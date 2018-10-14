3b56610a2a3ddcd01fd4a4f8d3d2fa375a2aae80
ItsDubC <ItsDubC@gmail.com>
2018-10-14 08:56:17 -0500

    Seed AssetTypes

---
+++ RentMyStuff.Data/Data/Migrations/20181014134944_PopulateAssetTypes.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RentMyStuff.Data.Migrations
+{
+    public partial class PopulateAssetTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO AssetTypes (Id, Name) VALUES (1, 'Movie')");
+            migrationBuilder.Sql("INSERT INTO AssetTypes (Id, Name) VALUES (2, 'Book')");
+            migrationBuilder.Sql("INSERT INTO AssetTypes (Id, Name) VALUES (3, 'Tool')");
+            migrationBuilder.Sql("INSERT INTO AssetTypes (Id, Name) VALUES (4, 'Power Tool')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
